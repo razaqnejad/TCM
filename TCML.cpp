a54f7250f5577795b353700f5e1084106ea88c13
Fatemeh Razaqnejad <71926921+razaqnejad@users.noreply.github.com>
2022-01-05 12:32:40 +0330

    v2.2
    exact and n depended inputs

--- TCML.cpp
+++ TCML.cpp
@@ -3,24 +3,33 @@
 #include <time.h>
 using namespace std;
 void makeDirs();
-void makeInputs(int,int,int,int);
-void makeAnInput(int,int,int,int);
+void makeInputs(int,bool,int,int,int);
+void makeAnInput(int,bool,int,int,int);
 void makeOutputs(int);
 void copyCode();
 void clear();
 int main(){
-    int n=10,m=1,a=1,b=10;
+    bool type=0;
+    int n=10,m=1,a=1;
+    int b=10;
     srand(time(NULL));
     cout << "number of test cases: ";
     cin >> n;
-    cout << "number of each test input numbers: ";
-    cin >> m;
+    cout << "0. n depended \t 1. exact n? ";
+    cin >> type;
+    if(type){
+        cout << "number of each test inputs: ";
+        cin >> m;
+    }else{
+        cout << "Maximum of each test inputs: ";
+        cin >> m;
+    }
     cout << "from? ";
     cin >> a;
     cout << "to? ";
     cin >> b;
     makeDirs();
-    makeInputs(n,m,a,b);
+    makeInputs(n,type,m,a,b);
     copyCode();
     makeOutputs(n);
     /**/cout << "making outputs compeleted\n";
@@ -33,16 +42,27 @@ void makeDirs(){
     system("mkdir problem && cd problem && mkdir in && mkdir out");
     /**/cout << "problem in and out dirs making compeleted\n";
 }
-void makeInputs(int n, int m, int a, int b){
+void makeInputs(int n, bool type, int m, int a, int b){
     for(int i=0 ; i<n ; i++){
-        makeAnInput(i,m,a,b);
+            makeAnInput(i,type,m,a,b);
+
     }
 }
-void makeAnInput(int i, int m, int a, int b){
+void makeAnInput(int i, bool type, int m, int a, int b){
+    int range= (type ? m : (rand()%m)+1);
     ofstream  file;
     file.open("./problem/in/input"+to_string(i+1)+".txt");
-    for (int j=0 ; j<m ; j++){
-        file << rand()%(b-a+1)+a;
+if(!type){
+        file << range <<'\n';
+    } 
+    for (int j=0 ; j<range ; j++){
+        if(!type){
+            for(int h=0 ; h<1 ; h++){   //if you need to add specific n number for each line chage the condition
+                file << rand()%(b-a+1)+a << ' ';
+            }
+        }else{
+            file << rand()%(b-a+1)+a;
+        }
         file << '\n';
     }
     file.close();
